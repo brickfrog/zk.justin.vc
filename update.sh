3726303962e7763af330fcacc663e4e2b474a47d
Justin Malloy <justin.t.malloy@gmail.com>
2022-04-08 20:04:34 -0400

    2022/04/08 changes

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 # Script to automatically update site, using cron from within wsl
 
-pushd /home/justin/coding/justin.vc/ && tzk build public && tzk commit;
+pushd /home/justin/coding/justin.vc/ && /home/justin/anaconda3/bin/tzk build public && /home/justin/anaconda3/bin/tzk commit;
 popd
